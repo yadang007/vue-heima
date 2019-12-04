178ace2c43018488375b2e94147e4fe8e8340cb1
yadang007 <838841674>
2019-12-04 15:08:02 +0800

    更新轮播图，六宫格，路由

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class=" app-container">
+    <mt-header fixed title="我的VUE项目"></mt-header>
+
+    <!-- 中间的 路由 router-view 区域 -->
+    <transition>
+      <router-view></router-view>
+    </transition>
+
+
+    <!-- 底部 Tabbar 区域 -->
+    <nav class="mui-bar mui-bar-tab">
+      <router-link class="mui-tab-item" to="/home">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </router-link>
+      <router-link class="mui-tab-item" to="/member">
+        <span class="mui-icon mui-icon-contact"></span>
+        <span class="mui-tab-label">会员</span>
+      </router-link>
+      <router-link class="mui-tab-item" to="/shopcar">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+					<span class="mui-badge">0</span>
+				</span>
+        <span class="mui-tab-label">购物车</span>
+      </router-link>
+      <router-link class="mui-tab-item" to="/search">
+        <span class="mui-icon mui-icon-search"></span>
+        <span class="mui-tab-label">搜索</span>
+      </router-link>
+    </nav>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+<style lang="scss" scoped>
+.app-container{
+  padding-top: 40px;
+  overflow-x: hidden;
+}
+
+.v-enter {
+  opacity: 0;
+  transform: translateX(100%);
+}
+
+.v-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+  position: absolute;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
+// 入口文件
+import Vue from  'vue'
+//导入APP
+import app from  './App.vue'
+//导入Mint-ui组件
+import {Header,Swipe, SwipeItem} from 'mint-ui'
+//注册
+Vue.component(Header.name,Header)
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+//导入MUI的样式
+import './lib/mui/css/mui.min.css'
+//导入样式
+import  './lib/mui/css/icons-extra.css'
+//导入路由模块
+import VueRouter from "vue-router";
+//使用路由
+Vue.use(VueRouter)
+//导入自己的路由模块
+import router  from "./router.js";
+//导入vue-resources
+import VueResources from 'vue-resource'
+//使用
+Vue.use(VueResources)
 
-Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+var  vm =new Vue({
+    el:'#app',
+    render: c => c(app),
+    router
+})
