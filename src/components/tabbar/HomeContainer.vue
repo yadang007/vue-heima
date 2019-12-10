7e77ccbbd0acf1c1ed6c831e3bfedf4ee3964676
yadang007 <838841674>
2019-12-10 13:49:10 +0800

    购物车

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -1,14 +1,9 @@
 <template>
   <div>
 
-      <!-- 轮播图区域 -->
-      <mt-swipe :auto="4000">
-          <!-- 在组件中，使用v-for循环的话，一定要使用 key -->
-          <mt-swipe-item v-for="item in lunbotuList" :key="item.url">
-              <img :src="item.img" alt="">
-          </mt-swipe-item>
-      </mt-swipe>
+       <!--轮播图区域 -->
 
+      <swiper :lunbotuList="lunbotuList"></swiper>
       <!-- 九宫格 到 6宫格 的改造工程 -->
       <ul class="mui-table-view mui-grid-view mui-grid-9">
           <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
@@ -16,11 +11,13 @@
               <img src="../../images/menu1.png" alt="">
               <div class="mui-media-body">新闻资讯</div></router-link></a></li>
           <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <router-link to="/home/photolist">
               <img src="../../images/menu2.png" alt="">
-              <div class="mui-media-body">图片分享</div></a></li>
+                  <div class="mui-media-body">图片分享</div></router-link></a></li>
           <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+              <router-link to="/home/goodslist">
               <img src="../../images/menu3.png" alt="">
-              <div class="mui-media-body">商品购买</div></a></li>
+                  <div class="mui-media-body">商品购买</div></router-link></a></li>
           <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
               <img src="../../images/menu4.png" alt="">
               <div class="mui-media-body">留言反馈</div></a></li>
@@ -39,6 +36,8 @@
 
 <script>
     import { Toast } from "mint-ui";
+    //导入轮播图组件
+    import swiper from "../subcomponents/swiper.vue";
 
     export default {
         data() {
@@ -65,30 +64,15 @@
                     }
                 });
             }
+        },
+        components:{
+            swiper
         }
     };
 </script>
 
 <style lang="scss" scoped>
-.mint-swipe{
-    height: 200px;
-    .mint-swipe-item {
-        &:nth-child(1) {
-            background-color: red;
-        }
-        &:nth-child(2) {
-            background-color: blue;
-        }
-        &:nth-child(3) {
-            background-color: cyan;
-        }
 
-        img {
-            width: 100%;
-            height: 100%;
-        }
-    }
-}
 .mui-grid-view.mui-grid-9 {
     background-color: #fff;
     border: none;
--- src/router.js
+++ src/router.js
@@ -7,6 +7,12 @@ import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'
 import NewsList from './components/news/Newslist.vue'
 import NewsInfow from "./components/news/NewsInfo.vue";
+import PhotoList from './components/photos/PhotoList.vue'
+import PhotoInfo from "./components/photos/PhotoInfo.vue"
+import GoodsList from './components/goods/GoodsList.vue'
+import GoodsInfo from "./components/goods/GoodsInfo.vue";
+import GoodsDesc from  './components/goods/GoodsDesc.vue'
+import GoodsComment from  './components/goods/GoodsComment.vue'
 
 // 3. 创建路由对象
 var router = new VueRouter({
@@ -17,7 +23,13 @@ var router = new VueRouter({
     { path: '/shopcar', component: ShopcarContainer },
     { path: '/search', component: SearchContainer },
     { path: '/home/newslist', component: NewsList },
-    { path: '/home/newsinfo/:id', component: NewsInfow }
+    { path: '/home/newsinfo/:id', component: NewsInfow },
+    { path: '/home/photolist', component: PhotoList },
+    { path: '/home/photoinfo/:id', component: PhotoInfo },
+    { path: '/home/goodslist', component: GoodsList },
+    { path: '/home/goodsinfo/:id', component: GoodsInfo ,name:'goodsinfo'},
+    { path: '/home/goodsdesc/:id', component: GoodsDesc ,name:'goodsdesc'},
+    { path: '/home/goodscomment/:id', component: GoodsComment ,name:'goodscomment'}
   ],
   linkActiveClass: 'mui-active' // 覆盖默认的路由高亮的类，默认的类叫做 router-link-active
 })
